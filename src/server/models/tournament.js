e5e77bd35facdcb5d22eb388863770c564aa290e
Jonathan.Hammond <Jonathan.Hammond85@gmail.com>
2015-11-13 12:00:50 -0600

    Added nested document for tournament location instead of just a string.

--- src/server/models/tournament.js
+++ src/server/models/tournament.js
@@ -11,8 +11,26 @@ module.exports = function (mongoose, config) {
 	level: {type: Number, required: true},
 	date: {type: Date, required: true},
 	year: {type: Number, required: true},
-	location: {type: String, required: true}
+	location: {
+		name: {type: String},
+		address: {
+			street: {type: String},
+			city: {type: String},
+			state: {type: String},
+			zip: {type: String}
+		}
+	},
+	createdAt: {type: Date},
+	updatedAt: {type: Date}
   }, {versionKey: false});
+  
+  tournamentSchema.pre('save', function(next){
+	  var currentDate = new Date();
+	  
+	  this.updatedAt = currentDate;
+	  if(!this.createdAt)
+	  	this.createdAt = currentDate;
+  });
   
   return mongoose.model('tournament', tournamentSchema)
 };
