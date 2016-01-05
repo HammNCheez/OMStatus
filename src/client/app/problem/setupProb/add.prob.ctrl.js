a31ae5dca8c5b8992b9a3842b91722b701a33cfb
Jonathan Hammond <stapleman527@gmail.com>
2016-01-05 22:34:19 +0000

    Added tournaments dropdown to add problem screen.

--- src/client/app/problem/setupProb/add.prob.ctrl.js
+++ src/client/app/problem/setupProb/add.prob.ctrl.js
@@ -3,22 +3,34 @@
   angular
     .module('app.problem')
     .controller('AddProbController', AddProbController);
-  AddProbController.$inject = ['$uibModalInstance', 'problems'];
+  AddProbController.$inject = ['$uibModalInstance', '$http', 'problems'];
   /* @ngInject */
-  function AddProbController($uibModalInstance, problems) {
+  function AddProbController($uibModalInstance, $http, problems) {
 
     var vm = this;
     vm.problems = problems;
     vm.prob = {year: new Date().getFullYear()};
-
-    vm.ok = function () {
-      
-      
+    vm.ok = ok;
+    vm.cancel = cancel;
+    vm.tournaments = [];
+    
+    function ok() {
       $uibModalInstance.close(vm.prob);
     };
 
-    vm.cancel = function () {
+    function cancel() {
       $uibModalInstance.dismiss('cancel');
     };
+    
+    function loadTournaments() {      
+      $http.get('/api/tournaments').success(
+        function(data) {
+          _.forEach(data, function(value){
+            vm.tournaments.push({id:value._id, name:value.name});
+          });
+        });
+    };
+    
+    loadTournaments();
   };
 })();
